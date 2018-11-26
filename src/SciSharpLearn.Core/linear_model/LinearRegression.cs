290344294e2d72b49685670b8a61ba1c56c00200
Oceania2018 <haiping008@gmail.com>
2018-11-25 21:45:58 -0600

    linear_model.fit in progress, scipy.linalg is prerequisite

---
+++ src/SciSharpLearn.Core/linear_model/LinearRegression.cs
@@ -0,0 +1,20 @@
+﻿using NumSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SciSharpLearn.Core.linear_model
+{
+    public class LinearRegression
+    {
+        /// <summary>
+        /// Fit linear model.
+        /// </summary>
+        /// <param name="X">Training data</param>
+        /// <param name="y">Target values. Will be cast to X’s dtype if necessary</param>
+        public LinearRegression fit(NDArray X, NDArray y)
+        {
+            return this;
+        }
+    }
+}
---
+++ test/SciSharpLearn.UnitTest/linear_model/LinearRegressionTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SciSharpLearn.UnitTest.linear_model
+{
+    [TestClass]
+    public class LinearRegressionTest
+    {
+        NumPy np = new NumPy();
+
+        [TestMethod]
+        public void LinearRegression()
+        {
+            var X = np.array(new int[][]
+            {
+                new int[] { 1, 1 },
+                new int[] { 1, 2 },
+                new int[] { 2, 2 },
+                new int[] { 2, 3 }
+            });
+
+            var y = np.dot(X, np.array(new int[] { 1, 2 })) + 3;
+
+            var reg = LinearRegression().fit(X, y);
+        }
+    }
+}
